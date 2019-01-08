4aea2a598e824a84f145ede9e7a3ba74ed893806
rajatrao777 <rajatrao777@gmail.com>
2019-01-08 23:23:01 +0530

    added Redux-Form Code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ContactForm from './Contact'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    submit = values => {
+        // print the form values to the console
+        console.log(values);
+    }
+    render() {
+        return (<ContactForm onSubmit={this.submit} />);
+    }
 }
-
 export default App;
