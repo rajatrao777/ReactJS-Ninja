ff30a933db099a7102d791bb08126b8af10a72f0
rajatrao777 <rajatrao777@gmail.com>
2019-01-21 13:20:44 +0530

    Updates

--- precache-manifest.602890ccb4d475fa906fc16a2ee3e959.js
+++ precache-manifest.602890ccb4d475fa906fc16a2ee3e959.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ReactJS-Ninja/static/js/runtime~main.17ee0f02.js"
   },
   {
-    "revision": "70e1be94719d79424685",
-    "url": "/ReactJS-Ninja/static/js/main.70e1be94.chunk.js"
+    "revision": "49f191213c4614ff99c7",
+    "url": "/ReactJS-Ninja/static/js/main.49f19121.chunk.js"
   },
   {
     "revision": "f628ec22058ee0c475e4",
     "url": "/ReactJS-Ninja/static/js/1.f628ec22.chunk.js"
   },
   {
-    "revision": "70e1be94719d79424685",
-    "url": "/ReactJS-Ninja/static/css/main.237abfa5.chunk.css"
+    "revision": "49f191213c4614ff99c7",
+    "url": "/ReactJS-Ninja/static/css/main.93f2bcef.chunk.css"
   },
   {
-    "revision": "463d3e43894db9ab84efb53f301f9505",
+    "revision": "c2dd6eb3a925cdf4445bcb2fd0aed2ef",
     "url": "/ReactJS-Ninja/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactJS-Ninja/precache-manifest.20aae132e31fb0886f27152f2f5d5aaf.js"
+  "/ReactJS-Ninja/precache-manifest.602890ccb4d475fa906fc16a2ee3e959.js"
 );
 
 workbox.clientsClaim();
