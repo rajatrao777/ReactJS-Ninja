da93ca2338841aeeaf98801f02c5b2fce4b89713
rajatrao777 <rajatrao777@gmail.com>
2019-01-22 15:45:14 +0530

    added the home page landing code..might need some changes..refactor later :(

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,17 @@ class Home extends React.Component {
       <div>
         {/* <img src={background} alt="img failed loading" /> */}
         <div className="imageback" />
+        <div className="header--company">Stortal</div>
+        <a href="#">
+          <div className="header--signup">SIGNUP</div>
+        </a>
+        <a href="#">
+          <div className="header--login">LOGIN</div>
+        </a>
+        <div className="section--values">
+          A New way to <br />
+          connect your colleagues
+        </div>
       </div>
     );
   }
